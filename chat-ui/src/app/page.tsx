33cceb9284b4d097f1b096342c8edb81e638e1b6
ifrah-ashraf <ifrahashraf48@gmail.com>
2024-11-25 23:42:04 +0530

    created user, Data model

--- chat-ui/src/app/page.tsx
+++ chat-ui/src/app/page.tsx
@@ -1,24 +1,57 @@
 "use client"
 
+
 import { useEffect, useState } from "react"
 
+
+interface Data {
+  from : string,
+  to : string ,
+  message : string
+}
+
 export default function Home() {
+
+  const [rcvId , setRcvId] = useState("")
   const [message , setMessage] = useState("")
   const [ws , setWs] = useState<WebSocket | null >(null)
 
   const handleClick = () => {
-    if (ws){
-      ws.send(message)
-      console.log(message)
+    if (ws && ws.readyState === WebSocket.OPEN){
+        const payload: Data = {
+        from:"user-A",
+        to :rcvId , 
+        message : message
+      }
+
+      ws.send(JSON.stringify(payload))
+      console.log("Sent:", payload);
+
+      setMessage("")
     }else{
-      alert("lmao connection is closed")
       console.error("WebSocket connection is not established.");
     }
   }
 
   useEffect( () => {
-    const socket = new WebSocket("ws://localhost:8080/")
+    const socket = new WebSocket("ws://localhost:8080/chat")
     setWs(socket)
+
+    socket.onopen = ()=>{
+      console.log("Connection established")
+    }
+
+    socket.onclose = () => {
+      console.log("Connection has been closed")
+    }
+
+    socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    socket.onmessage = (event) => {
+      console.log("Message received:", event.data);
+    };
 
     return () =>{
       socket.close()
@@ -27,7 +60,15 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center ">
-      <h1 className="text-2xl font-bold text-center text-blue-400">Live chat application</h1>
+      <h1 className="text-2xl font-bold text-center text-indigo-600">Live chat application</h1>
+
+      <input 
+      type="text"
+      value={rcvId}
+      onChange={(e)=> setRcvId(e.target.value)}
+      placeholder="Enter the user-ID"
+      className="p-4 border-black border-2  mt-4 w-auto mb-4"
+      />
       
       <input 
       type="text"
@@ -39,7 +80,19 @@ export default function Home() {
 
       <button 
       onClick={handleClick}
-      className="p-2 border-black border-2 border-solid w-auto"
+      className="bg-blue-500 
+                text-white 
+                font-bold 
+                py-2 
+                px-4 
+                rounded 
+                shadow-md 
+                transition 
+                duration-150 
+                ease-in-out 
+                hover:bg-blue-600 
+                active:scale-95 
+                active:shadow-sm"
       >Send message</button>
 
     </div>
--- server/main.go
+++ server/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
+
+/* type Users struct {
+	ID         string
+	connection *websocket.Conn
+} */
+
+type Data struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	message string `json:"message"`
+}
+
+//var clients = make(map[string]*websocket.Conn)
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -17,37 +31,52 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	http.HandleFunc("/", wsHandler)
+	http.HandleFunc("/chat", wsHandler)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
 
+	/* userId := r.URL.Query().Get("id")
+	fmt.Println("user ID => ", userId) */
+
+	var message Data
+
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
+	/* clients[userId] = conn
+	// receiver user Id
+	rcv := message.To
+	rcvConn := clients[rcv] */
+
 	defer conn.Close()
 
 	for {
 		mt, msg, err := conn.ReadMessage()
-
 		if err != nil {
 			log.Println("Error while reading the message", err.Error())
 			return
 		}
 
+		err = json.Unmarshal(msg, &message)
+		if err != nil {
+			fmt.Println("Error: while unmarshaling the json data", err)
+			return
+		}
+
 		//fmt.Println("Received message:", string(msg))
-
-		go Readmsg(msg)
-
 		err = conn.WriteMessage(mt, msg)
 		if err != nil {
 			log.Println(err)
 			return
 		}
+
+		go Readmsg(msg)
 
 	}
 }
